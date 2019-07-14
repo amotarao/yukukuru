f7378d5fa66e0a97ab6f05509d33b32ab8cb3f3f
Amon Sawamura <amon.sawamura@gmail.com>
2019-07-14 13:01:43 +0900

    feat(onFirestoreUpdateUser): add functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions';
-import onCreateUserHandler from './functions/auth/onCreateUser';
 import getFollowersHandler from './api/getFollowers';
+import onCreateUserHandler from './functions/auth/onCreateUser';
+import onFirestoreUpdateUserHandler from './functions/firestore/onUpdateUser';
 
 export const getFollowers = functions.region('asia-northeast1').https.onRequest(async (_, res) => {
   await getFollowersHandler();
@@ -11,3 +12,8 @@ export const onCreateUser = functions
   .region('asia-northeast1')
   .auth.user()
   .onCreate(onCreateUserHandler);
+
+export const onFirestoreUpdateUser = functions
+  .region('asia-northeast1')
+  .firestore.document('users/{userId}')
+  .onUpdate(onFirestoreUpdateUserHandler);
