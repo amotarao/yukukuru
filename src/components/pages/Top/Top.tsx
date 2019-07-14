8959c4c53ace75afc8dc0b908d7927fca7734ff0
Amon Sawamura <amon.sawamura@gmail.com>
2019-07-14 13:48:17 +0900

    feat(app): alpha version

--- src/components/pages/Top/Top.tsx
+++ src/components/pages/Top/Top.tsx
@@ -8,11 +8,23 @@ export interface TopProps {
   signIn: () => Promise<void>;
 }
 
-export const Top: React.FC<TopProps> = ({ className, signIn }) => {
-  return (
+export const Top: React.FC<TopProps> = ({ className, isLoading, signIn }) => {
+  return isLoading ? (
+    <p>読み込み中</p>
+  ) : (
     <div className={className}>
-      <h1>Follower Manager</h1>
-      <button onClick={signIn}>ログイン</button>
+      <h1>Follower Manager (仮) alpha</h1>
+      <p>人事ったーの後釜を目指しました</p>
+      <p>alpha版のため、うまく動作しない場合があります</p>
+      <p>サイトのデザインがされていませんが、あとでいい感じにやります</p>
+      <p>
+        <a href="https://github.com/amotarao/follower-manager" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+      </p>
+      <button onClick={signIn}>
+        <img src="https://developer.twitter.com/content/dam/developer-twitter/images/sign-in-with-twitter-gray.png" alt="Sign in with Twitter" />
+      </button>
     </div>
   );
 };
