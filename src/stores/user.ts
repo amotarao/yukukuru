178eb7c76fc4560350d2ed6935db01889a890b02
Amon Sawamura <amon.sawamura@gmail.com>
2019-10-09 00:48:52 +0900

    fix(user): fix set token processing

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,54 +1,40 @@
 import { useState, useEffect } from 'react';
 import { createContainer } from 'unstated-next';
 import { auth, firestore, provider } from '../modules/firebase';
+import { TokenDataInterface } from './database/token';
 
-interface CredentialInterface {
-  twitterAccessToken: string;
-  twitterAccessTokenSecret: string;
-  twitterId: string;
-}
+const tokensCollection = firestore.collection('tokens');
 
 const useUser = () => {
   const [isLoading, setLoading] = useState<boolean>(true);
   const [signedIn, setSignedIn] = useState<boolean>(false);
   const [user, setUser] = useState<firebase.User | null>(null);
-  const [data, setData] = useState<CredentialInterface | null>(null);
+  const [token, setToken] = useState<TokenDataInterface | null>(null);
 
   useEffect(() => {
-    auth
-      .getRedirectResult()
-      .then(({ additionalUserInfo, credential, user }) => {
-        if (additionalUserInfo && credential && user) {
-          const twitterAccessToken = (credential as any).accessToken;
-          const twitterAccessTokenSecret = (credential as any).secret;
-          setData({ twitterAccessToken, twitterAccessTokenSecret, twitterId: (additionalUserInfo.profile! as any).id_str! });
-        }
-        setUser(user);
-      })
-      .catch(() => {
-        setUser(null);
-        setData(null);
-      });
-  }, []);
+    auth.getRedirectResult().then(({ additionalUserInfo, credential, user }) => {
+      if (additionalUserInfo && credential && user) {
+        setToken({
+          twitterAccessToken: (credential as any).accessToken,
+          twitterAccessTokenSecret: (credential as any).secret,
+          twitterId: (additionalUserInfo.profile! as any).id_str!,
+        });
+      }
+    });
 
-  useEffect(() => {
     auth.onAuthStateChanged((user) => {
       setUser(user);
-      setSignedIn(Boolean(user));
+      setSignedIn(user !== null);
       setLoading(false);
     });
   }, []);
 
   useEffect(() => {
-    (async () => {
-      if (data && user) {
-        await firestore
-          .collection('tokens')
-          .doc(user.uid)
-          .set(data);
-      }
-    })();
-  }, [data, user]);
+    if (!token || !user) {
+      return;
+    }
+    tokensCollection.doc(user.uid).set(token);
+  }, [token, user]);
 
   const signIn = () => auth.signInWithRedirect(provider);
   const signOut = () => auth.signOut();
