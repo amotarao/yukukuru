44f2a3d299eb096fc3798f1fd294d344bfa18aee
Amon Sawamura <amon.sawamura@gmail.com>
2020-01-04 03:24:21 +0900

    refactor(f): users methods move to utils

--- functions/src/functions/firestore/onUpdateToken.ts
+++ functions/src/functions/firestore/onUpdateToken.ts
@@ -1,14 +1,9 @@
 import * as functions from 'firebase-functions';
-import { firestore } from '../../modules/firebase';
 import { TokenData } from '../../utils/interfaces';
+import { updateUserInvalid } from '../../utils/firestore/users';
 
 export default async ({ after }: functions.Change<FirebaseFirestore.DocumentSnapshot>) => {
   const { twitterAccessToken = null, twitterAccessTokenSecret = null, twitterId = null } = after.data() as TokenData;
-
   const invalid = !twitterAccessToken || !twitterAccessTokenSecret || !twitterId;
-
-  await firestore
-    .collection('users')
-    .doc(after.id)
-    .set({ invalid }, { merge: true });
+  await updateUserInvalid(after.id, invalid);
 };
