bbdcc08c5829bd0b3bfdc58ca44ecaa6725638e2
Amon Sawamura <amon.sawamura@gmail.com>
2020-01-27 22:26:56 +0900

    refactor(h: records): summary get function

--- src/stores/database/records.ts
+++ src/stores/database/records.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { createContainer } from 'unstated-next';
 import firebase, { firestore } from '../../modules/firebase';
 import { convertRecords } from '../../utils/records';
@@ -47,7 +47,7 @@ const convertRecordItems = (snapshot: firebase.firestore.QueryDocumentSnapshot) 
   return item;
 };
 
-const getRecords = async (uid: string, end: firebase.firestore.Timestamp): Promise<firebase.firestore.QuerySnapshot> => {
+const getRecordsFromFirestore = async (uid: string, end: firebase.firestore.Timestamp): Promise<firebase.firestore.QuerySnapshot> => {
   const qs = await usersCollection
     .doc(uid)
     .collection('records')
@@ -71,59 +71,64 @@ const useRecords = () => {
   /** アイテム */
   const [items, setItems] = useState<RecordViewInterface[]>([]);
 
-  /** アテムがあるかどうか */
-  const [hasItems, setHasItems] = useState<boolean>(false);
-
   /** Firebase UID */
   const [uid, setUid] = useState<string | null>(null);
 
   /** アイテムの読み込みのカーソル代わり */
   const [lastDurationEnd, setLastDurationEnd] = useState<firebase.firestore.Timestamp>(firebase.firestore.Timestamp.now());
 
+  /**
+   * Records を取得し処理する
+   */
+  const getRecords = useCallback(async () => {
+    if (!uid) {
+      return;
+    }
+    const { docs, size } = await getRecordsFromFirestore(uid, lastDurationEnd);
+    const tmpItems = docs.map(convertRecordItems).sort((a, b) => b.data.durationEnd.seconds - a.data.durationEnd.seconds);
+    const [newItems, newLastDurationEnd] = convertRecords(tmpItems);
+
+    setItems([...items, ...newItems]);
+    setLastDurationEnd(newLastDurationEnd);
+    setHasNext(size >= 20);
+
+    setLoading(false);
+    setNextLoading(false);
+  }, [items, lastDurationEnd, uid]);
+
+  /**
+   * 初回 Records を取得する
+   */
+  useEffect(() => {
+    if (isLoading || !uid) {
+      return;
+    }
+    getRecords();
+  }, [getRecords, isLoading, uid]);
+
+  /**
+   * 続きの Records を取得する
+   */
   const getNextRecords = () => {
     if (isNextLoading || !uid) {
       return;
     }
     setNextLoading(true);
-
-    (async () => {
-      const { docs, size } = await getRecords(uid, lastDurationEnd);
-      const tmpItems = docs.map(convertRecordItems).sort((a, b) => b.data.durationEnd.seconds - a.data.durationEnd.seconds);
-      const [newItems, newLastDurationEnd] = convertRecords(tmpItems);
-
-      setItems([...items, ...newItems]);
-      setLastDurationEnd(newLastDurationEnd);
-      setHasNext(size >= 20);
-
-      setNextLoading(false);
-    })();
+    getRecords();
   };
 
   useEffect(() => {
-    if (!uid) {
+    if (!isLoading || !uid) {
       return;
     }
-
-    (async () => {
-      const { docs, size } = await getRecords(uid, firebase.firestore.Timestamp.now());
-      const tmpItems = docs.map(convertRecordItems).sort((a, b) => b.data.durationEnd.seconds - a.data.durationEnd.seconds);
-      const [newItems, newLastDurationEnd] = convertRecords(tmpItems);
-
-      setItems(newItems);
-      setLastDurationEnd(newLastDurationEnd);
-      setHasItems(size > 0);
-      setHasNext(size >= 20);
-
-      setLoading(false);
-      setNextLoading(false);
-    })();
-  }, [uid]);
+    getRecords();
+  }, [getRecords, isLoading, uid]);
 
   return {
     isLoading,
     isNextLoading,
     items,
-    hasItems,
+    hasItems: items.length > 0,
     hasNext,
     setUid,
     getNextRecords,
