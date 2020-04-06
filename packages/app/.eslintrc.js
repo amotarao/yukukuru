fbbd00b3b6db0dfae82510f66d43e22df8a448df
Amon Sawamura <amon.sawamura@gmail.com>
2020-04-06 22:57:11 +0900

    chore(app): extend root eslintrc

--- packages/app/.eslintrc.js
+++ packages/app/.eslintrc.js
@@ -1,54 +1,32 @@
 module.exports = {
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
+  extends: [
+    '../../.eslintrc.js',
+    'plugin:react/recommended',
   ],
-  "plugins": [
-    "@typescript-eslint",
-    "react",
-    "react-hooks",
-  ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  plugins: ['react', 'react-hooks'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "sourceType": "module",
-    "project": "./tsconfig.json",
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true,
-    "jest": true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
   },
-  "rules": {
-    "no-unused-vars": "off",
-    "no-console": "off",
-    "@typescript-eslint/no-unused-vars": "error",
-    "react/prop-types": "off",
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "prettier/prettier": [
-      "error",
-      {
-        "singleQuote": true,
-        "semi": true,
-        "trailingComma": "es5",
-        "printWidth": 160,
-        "arrowParens": "always",
-      },
-    ],
-    "indent": [
-      "error",
-      2,
-      {
-        "SwitchCase": 1,
-      },
-    ],
-    "linebreak-style": ["error", "unix"],
-    "quotes": ["error", "single"],
-    "semi": ["error", "always"],
+  rules: {
+    'no-unused-vars': 'off',
+    'no-console': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/indent': ['warn', 2],
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-use-before-define': 'warn',
+    'react/prop-types': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
-}
+};
