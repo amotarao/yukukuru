9028c5ae888750dbe817f23c3073f092e216daaf
Amon Sawamura <amon.sawamura@gmail.com>
2020-04-12 03:08:46 +0900

    feat(getFollowers): add getTokens, use getAll

--- packages/functions/src/api/getFollowers.ts
+++ packages/functions/src/api/getFollowers.ts
@@ -2,7 +2,7 @@ import { setUserResult } from '../utils/firestore/users/setUserResult';
 import { setUserResultWithNoChange } from '../utils/firestore/users/setUserResultWithNoChange';
 import { tmpGetTargetsToGetFollowers } from '../utils/firestore/users/tmpGetTargetsToGetFollowers';
 import { addWatch } from '../utils/firestore/users/watches/addWatch';
-import { getToken } from '../utils/firestore/tokens/getToken';
+import { getTokens } from '../utils/firestore/tokens/getTokens';
 import { setTokenInvalid } from '../utils/firestore/tokens/setTokenInvalid';
 import { getFollowersIds } from '../utils/twitter/getFollowersIds';
 import { checkInvalidToken, checkProtectedUser } from '../utils/twitter/error';
@@ -11,25 +11,27 @@ import { TwitterAccessToken } from '../utils/twitter/generateClient';
 export default async (): Promise<void> => {
   const now = new Date();
   const targets = await tmpGetTargetsToGetFollowers();
+  const tokens = await getTokens(targets.map((doc) => doc.id));
 
-  const requests = targets.map(async (doc) => {
+  const requests = targets.map(async (doc, i) => {
     const { nextCursor } = doc.data;
 
-    const token = await getToken(doc.id);
-    if (!token) {
+    const token = tokens[i];
+    if (token === null) {
       console.log(doc.id, 'no-token');
       await setTokenInvalid(doc.id);
       return;
     }
-    const { twitterAccessToken, twitterAccessTokenSecret, twitterId } = token;
 
+    /* eslint-disable @typescript-eslint/camelcase */
     const twitterToken: TwitterAccessToken = {
-      access_token_key: twitterAccessToken,
-      access_token_secret: twitterAccessTokenSecret,
+      access_token_key: token.twitterAccessToken,
+      access_token_secret: token.twitterAccessTokenSecret,
     };
+    /* eslint-enable @typescript-eslint/camelcase */
 
     const result = await getFollowersIds(twitterToken, {
-      userId: twitterId,
+      userId: token.twitterId,
       cursor: nextCursor,
       count: 10000, // Firestore ドキュメント データサイズ制限を考慮した数値
     });
---
+++ packages/functions/src/utils/firestore/tokens/getTokens.ts
@@ -0,0 +1,23 @@
+import { TokenData } from '@yukukuru/types';
+import { firestore } from '../../../modules/firebase';
+
+const collection = firestore.collection('tokens');
+
+export const getTokens = async (ids: string[]): Promise<(TokenData | null)[]> => {
+  const refs = ids.map((id) => collection.doc(id));
+  const options: FirebaseFirestore.ReadOptions = {
+    fieldMask: ['twitterAccessToken', 'twitterAccessTokenSecret', 'twitterId'],
+  };
+  const docs = await firestore.getAll(...refs, options);
+
+  return docs.map((doc) => {
+    if (!doc.exists) {
+      return null;
+    }
+    const { twitterAccessToken = null, twitterAccessTokenSecret = null, twitterId = null } = doc.data() as TokenData;
+    if (!twitterAccessToken || !twitterAccessTokenSecret || !twitterId) {
+      return null;
+    }
+    return { twitterAccessToken, twitterAccessTokenSecret, twitterId };
+  });
+};
