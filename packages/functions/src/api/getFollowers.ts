e82216b9013a54d3085c21c84d5e58e17905a6b4
Amon Sawamura <amon.sawamura@gmail.com>
2020-04-12 20:06:01 +0900

    feat(getFollower): use group on query

--- packages/functions/src/api/getFollowers.ts
+++ packages/functions/src/api/getFollowers.ts
@@ -3,23 +3,21 @@ import { firestore } from '../modules/firebase';
 import { addQueuesTypeGetFollowers } from '../utils/firestore/queues/addQueuesTypeGetFollowers';
 
 export default async (): Promise<void> => {
-  const now = new Date();
-  // API は 15分で 75000人 の取得制限がある
-  // 1回で 10000人 まで取れるので、2.5分間隔
-  // 余裕を見て 3分間隔
-  const time3 = new Date();
-  time3.setMinutes(now.getMinutes() - 3);
+  const now = new Date(Math.floor(new Date().getTime() / (60 * 1000)) * 60 * 1000);
+  const group = now.getMinutes() % 15;
 
-  const time15 = new Date();
-  time15.setMinutes(now.getMinutes() - 15);
+  // 15分前
+  const time15 = new Date(now.getTime() - 14 * 60 * 1000);
+  // 60分前
+  const time60 = new Date(now.getTime() - 59 * 60 * 1000);
 
   const allUsers = firestore
     .collection('users')
     .where('active', '==', true)
     .where('invalid', '==', false)
-    .where('lastUpdated', '<', time15)
-    .orderBy('lastUpdated')
-    .limit(100)
+    .where('pausedGetFollower', '==', false)
+    .where('lastUpdated', '<', time60)
+    .where('group', '==', group)
     .get();
 
   const pausedUsers = firestore
@@ -27,9 +25,8 @@ export default async (): Promise<void> => {
     .where('active', '==', true)
     .where('invalid', '==', false)
     .where('pausedGetFollower', '==', true)
-    .where('lastUpdated', '<', time3)
-    .orderBy('lastUpdated')
-    .limit(10)
+    .where('lastUpdated', '<', time15)
+    .where('group', '==', group)
     .get();
 
   const newUsers = firestore
@@ -37,7 +34,6 @@ export default async (): Promise<void> => {
     .where('active', '==', true)
     .where('invalid', '==', false)
     .where('newUser', '==', true)
-    .limit(10)
     .get();
 
   const [allUsersSnap, pausedUsersSnap, newUsersSnap] = await Promise.all([allUsers, pausedUsers, newUsers]);
