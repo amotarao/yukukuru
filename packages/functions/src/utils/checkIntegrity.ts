21251b9a49dee47f4c71d6250fccf531fd1255ba
Amon Sawamura <amon.sawamura@gmail.com>
2020-04-22 00:20:11 +0900

    chore(functions): fix log

--- packages/functions/src/utils/checkIntegrity.ts
+++ packages/functions/src/utils/checkIntegrity.ts
@@ -4,7 +4,7 @@ import { updateUserCheckIntegrity } from './firestore/users/integrity';
 import { getRecords } from './firestore/records/getRecords';
 import { getWatches } from './firestore/watches/getWatches';
 import { removeWatches } from './firestore/watches/removeWatches';
-import { getDiffFollowers, getDiffRecords, DiffWithId } from './diff';
+import { getDiffFollowers, getDiffRecords, Diff, DiffWithId } from './diff';
 import { convertRecords } from './convert';
 
 type Props = {
@@ -38,8 +38,21 @@ const convertWatches = (watches: FirestoreIdData<WatchData>[]): { ids: string[];
   return convertedWatches;
 };
 
+const checkSameEnd = (diffsA: Diff[], diffsB: Diff[]): boolean => {
+  const stringify = (d: Diff): string => {
+    return JSON.stringify({
+      type: d.type,
+      uid: d.uid,
+      end: d.durationEnd.getTime(),
+    });
+  };
+  const a = diffsA.map(stringify).join();
+  const b = diffsB.map(stringify).join();
+  return a === b;
+};
+
 export const checkIntegrity = async ({ uid }: Props, now: Date): Promise<void> => {
-  const watches = convertWatches(await getWatches({ uid, count: 500 }));
+  const watches = convertWatches(await getWatches({ uid, count: 100 }));
 
   if (watches.length < 5) {
     return;
@@ -70,29 +83,23 @@ export const checkIntegrity = async ({ uid }: Props, now: Date): Promise<void> =
   // 存在すべきではないが何故か存在する差分
   const unknownDiffs = getDiffRecords(firestoreDiffs, currentDiffs);
 
-  console.log(JSON.stringify({ type: 'checkIntegrity', uid, notExistsDiffs, unknownDiffs }));
-
-  // Todo: 存在しないドキュメントがある場合は追加する
   if (notExistsDiffs.length !== 0 && unknownDiffs.length === 0) {
-    //
-  }
-
-  // 得体のしれないドキュメントがある場合はエラーを出す
-  if (notExistsDiffs.length === 0 && unknownDiffs.length !== 0) {
-    console.error(JSON.stringify({ type: 'checkIntegrity', uid, unknownDiffs }));
-  }
-
-  // 何も変化がない場合、そのまま削除する
-  if (notExistsDiffs.length === 0 && unknownDiffs.length === 0) {
+    // Todo: 存在しないドキュメントがある場合は追加する
+    console.log(JSON.stringify({ type: 'checkIntegrity: hasNotExistsDiffs', uid, notExistsDiffs }));
+  } else if (notExistsDiffs.length === 0 && unknownDiffs.length !== 0) {
+    // 得体のしれないドキュメントがある場合はエラーを出す
+    console.error(JSON.stringify({ type: 'checkIntegrity: hasUnknownDiffs', uid, unknownDiffs }));
+  } else if (notExistsDiffs.length === 0 && unknownDiffs.length === 0) {
+    // 何も変化がない場合、そのまま削除する
     const removeIds = _.flatten(watches.map(({ ids }) => ids).slice(0, watches.length - 1));
     await removeWatches({ uid, removeIds });
+  } else if (checkSameEnd(notExistsDiffs, unknownDiffs)) {
+    // Todo: durationStart だけ異なるドキュメントがある場合は、アップデートする
+    console.log(JSON.stringify({ type: 'checkIntegrity: sameEnd', uid }));
+  } else {
+    // 想定されていない処理
+    console.log(JSON.stringify({ type: 'checkIntegrity: ERROR', uid, notExistsDiffs, unknownDiffs }));
   }
-
-  // Todo: durationStart だけ異なるドキュメントがある場合は、アップデートする
-  // const a = unknownDiffs
-  // .map((diff) => firestoreDiffsWithId.find((diffWithId) => diffWithId.diff === diff))
-  // .map((diff) => ('id' in diff ? diff.id : null))
-  // .filter((d): d is string => d !== null);
 
   await updateUserCheckIntegrity(uid, now);
 };
