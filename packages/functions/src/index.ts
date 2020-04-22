47b901233c5cf0939d78c8c142b4b6aa98486372
Amon Sawamura <amon.sawamura@gmail.com>
2020-04-22 10:26:50 +0900

    chore(functions): add comment, change variable name

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -10,7 +10,7 @@ import { updateTokenHandler } from './handlers/updateToken';
 import { onCreateQueueHandler } from './functions/firestore/onCreateQueue';
 import { env } from './utils/env';
 
-const builder = functions.region('asia-northeast1');
+const functionsBase = functions.region('asia-northeast1');
 
 const httpsRuntimeOptions: functions.RuntimeOptions = {
   timeoutSeconds: 20,
@@ -27,7 +27,10 @@ const functionsRuntimeOptions: functions.RuntimeOptions = {
   memory: '1GB',
 };
 
-export const getFollowers = builder.runWith(httpsRuntimeOptions).https.onRequest((req, res) => {
+/**
+ * 定期処理: フォロワー取得
+ */
+export const getFollowers = functionsBase.runWith(httpsRuntimeOptions).https.onRequest((req, res) => {
   (async (): Promise<void> => {
     if (req.query.key !== env.http_functions_key) {
       res.status(403).end();
@@ -38,7 +41,10 @@ export const getFollowers = builder.runWith(httpsRuntimeOptions).https.onRequest
   })();
 });
 
-export const updateTwUsers = builder.runWith(oldHttpsRuntimeOptions).https.onRequest(async (req, res) => {
+/**
+ * 定期処理: Twitter ユーザー情報更新
+ */
+export const updateTwUsers = functionsBase.runWith(oldHttpsRuntimeOptions).https.onRequest(async (req, res) => {
   if (req.query.key !== env.http_functions_key) {
     res.status(403).end();
     return;
@@ -47,7 +53,10 @@ export const updateTwUsers = builder.runWith(oldHttpsRuntimeOptions).https.onReq
   res.status(200).end();
 });
 
-export const checkIntegrity = builder.runWith(httpsRuntimeOptions).https.onRequest((req, res) => {
+/**
+ * 定期処理: 整合性チェック
+ */
+export const checkIntegrity = functionsBase.runWith(httpsRuntimeOptions).https.onRequest((req, res) => {
   (async (): Promise<void> => {
     if (req.query.key !== env.http_functions_key) {
       res.status(403).end();
@@ -58,22 +67,42 @@ export const checkIntegrity = builder.runWith(httpsRuntimeOptions).https.onReque
   })();
 });
 
-export const onCreateUser = builder.auth.user().onCreate(onCreateUserHandler);
+/**
+ * Twitter Token のアップデート
+ */
+export const updateToken = functionsBase
+  .runWith({ timeoutSeconds: 10, memory: '256MB' })
+  .https.onCall(updateTokenHandler);
 
-export const onDeleteUser = builder.auth.user().onDelete(onDeleteUserHandler);
+/**
+ * Auth: ユーザーが作成されたときの処理
+ */
+export const onCreateUser = functionsBase.auth.user().onCreate(onCreateUserHandler);
 
-export const onFirestoreUpdateToken = builder.firestore
+/**
+ * Auth: ユーザーが削除されたときの処理
+ */
+export const onDeleteUser = functionsBase.auth.user().onDelete(onDeleteUserHandler);
+
+/**
+ * Firestore: トークンが更新されたときの処理
+ */
+export const onFirestoreUpdateToken = functionsBase.firestore
   .document('tokens/{userId}')
   .onUpdate(onFirestoreUpdateTokenHandler);
 
-export const updateToken = builder.runWith({ timeoutSeconds: 10, memory: '256MB' }).https.onCall(updateTokenHandler);
-
-export const onCreateWatch = builder
+/**
+ * Firestore: watch が作成されたときの処理
+ */
+export const onCreateWatch = functionsBase
   .runWith(functionsRuntimeOptions)
   .firestore.document('users/{userId}/watches/{watchId}')
   .onCreate(onCreateWatchHandler);
 
-export const onCreateQueue = builder
+/**
+ * Firestore: queue が作成されたときの処理
+ */
+export const onCreateQueue = functionsBase
   .runWith(functionsRuntimeOptions)
   .firestore.document('queues/{queueId}')
   .onCreate(onCreateQueueHandler);
