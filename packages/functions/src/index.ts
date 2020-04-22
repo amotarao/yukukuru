7f709a5987346c1ff1d65669f370593bd3fb6e09
Amon Sawamura <amon.sawamura@gmail.com>
2020-04-22 10:22:24 +0900

    feat(functions): replace onCreateWatchHandler (#81)
    * feat(functions): replace onCreateWatchHandler
    
    * feat(functions): add twitter 17 error

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -3,7 +3,7 @@ import getFollowersHandler from './api/getFollowers';
 import updateTwUsersHandler from './api/updateTwUsers';
 import onCreateUserHandler from './functions/auth/onCreateUser';
 import onDeleteUserHandler from './functions/auth/onDeleteUser';
-import onFirestoreUpdateUserHandler from './functions/firestore/onUpdateUser';
+import onCreateWatchHandler from './functions/firestore/onCreateWatch';
 import onFirestoreUpdateTokenHandler from './functions/firestore/onUpdateToken';
 import { updateTokenHandler } from './handlers/updateToken';
 import { onCreateQueueHandler } from './functions/firestore/onCreateQueue';
@@ -45,16 +45,16 @@ export const onCreateUser = builder.auth.user().onCreate(onCreateUserHandler);
 
 export const onDeleteUser = builder.auth.user().onDelete(onDeleteUserHandler);
 
-export const onFirestoreUpdateUser = builder
-  .runWith(functionsRuntimeOptions)
-  .firestore.document('users/{userId}')
-  .onUpdate(onFirestoreUpdateUserHandler);
-
 export const onFirestoreUpdateToken = builder.firestore
   .document('tokens/{userId}')
   .onUpdate(onFirestoreUpdateTokenHandler);
 
 export const updateToken = builder.runWith({ timeoutSeconds: 10, memory: '256MB' }).https.onCall(updateTokenHandler);
+
+export const onCreateWatch = builder
+  .runWith(functionsRuntimeOptions)
+  .firestore.document('users/{userId}/watches/{watchId}')
+  .onCreate(onCreateWatchHandler);
 
 export const onCreateQueue = builder
   .runWith(functionsRuntimeOptions)
