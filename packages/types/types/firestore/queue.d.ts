d13bfc06fda5ad2812a32563d5e485a86fc216fc
Amon Sawamura <amon.sawamura@gmail.com>
2020-04-24 09:58:43 +0900

    feat(types): add QueueTypeConvertRecordsData, v0.2.6

--- packages/types/types/firestore/queue.d.ts
+++ packages/types/types/firestore/queue.d.ts
@@ -1,3 +1,5 @@
+import { FirestoreDateLike, Timestamp } from '../firestore';
+
 export interface QueueTypeGetFollowersData {
   /** キュータイプ */
   type: 'getFollowers';
@@ -31,6 +33,23 @@ export interface QueueTypeUpdateTwUsersData {
   };
 }
 
-export type QueueData = QueueTypeGetFollowersData | QueueTypeCheckIntegrityData | QueueTypeUpdateTwUsersData;
+export interface QueueTypeConvertRecordsData<T extends FirestoreDateLike = Timestamp> {
+  /** キュータイプ */
+  type: 'convertRecords';
+
+  data: {
+    /** Firebase UID */
+    uid: string;
+
+    /** いつの日のデータから見るか */
+    cursor: T;
+  };
+}
+
+export type QueueData =
+  | QueueTypeGetFollowersData
+  | QueueTypeCheckIntegrityData
+  | QueueTypeUpdateTwUsersData
+  | QueueTypeConvertRecordsData;
 
 export type QueueType = QueueData['type'];
