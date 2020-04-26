7c8c5cabf10f6cf135affaac5bf0796ca6482cb0
Amon Sawamura <amon.sawamura@gmail.com>
2020-04-26 20:53:51 +0900

    feat(mui): render styles on ssr

--- packages/yukukuru-app/src/pages/_document.tsx
+++ packages/yukukuru-app/src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import { ServerStyleSheets } from '@material-ui/core/styles';
 import Document, { Head, Main, NextScript } from 'next/document';
 import React from 'react';
 import { GlobalStyle } from '../components/GlobalStyle';
@@ -45,3 +46,20 @@ gtag('config', ${process.env.GOOGLE_ANALYTICS});`,
     );
   }
 }
+
+MyDocument.getInitialProps = async (ctx): Promise<any> => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+  };
+};
