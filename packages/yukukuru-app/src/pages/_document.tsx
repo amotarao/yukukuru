8f015cd79c57b4de7b800d42d05e0b151405aad7
Amon Sawamura <amon.sawamura@gmail.com>
2020-04-26 22:56:10 +0900

    fix(mui): disabled mui style ssr

--- packages/yukukuru-app/src/pages/_document.tsx
+++ packages/yukukuru-app/src/pages/_document.tsx
@@ -46,20 +46,3 @@ gtag('config', ${process.env.GOOGLE_ANALYTICS});`,
     );
   }
 }
-
-MyDocument.getInitialProps = async (ctx): Promise<any> => {
-  const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
-
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-    });
-
-  const initialProps = await Document.getInitialProps(ctx);
-
-  return {
-    ...initialProps,
-    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
-  };
-};
