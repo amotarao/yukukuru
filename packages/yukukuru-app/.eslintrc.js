07701aa37733a3158290365f82b0677f41a29b74
Amon Sawamura <amon.sawamura@gmail.com>
2020-04-27 09:16:17 +0900

    chore(eslint): add rule, fix path

--- packages/yukukuru-app/.eslintrc.js
+++ packages/yukukuru-app/.eslintrc.js
@@ -35,5 +35,6 @@ module.exports = {
     'react/prop-types': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
   },
 };
