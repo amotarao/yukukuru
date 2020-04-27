3c95ba6116b2df8d6332fb588a4f149915f61cc8
Amon Sawamura <amon.sawamura@gmail.com>
2020-04-27 10:05:24 +0900

    chore(app): add pwa configs

--- packages/yukukuru-app/next.config.js
+++ packages/yukukuru-app/next.config.js
@@ -1,6 +1,10 @@
+const withPWA = require('next-pwa');
 require('dotenv').config();
 
-module.exports = {
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+  },
   exportPathMap() {
     return {
       '/': { page: '/' },
@@ -18,4 +22,4 @@ module.exports = {
     FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
     FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
   },
-};
+});
