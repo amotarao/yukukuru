9adf134a0b1625466e0b3c5f65a5b1eb06c36299
Amon Sawamura <amon.sawamura@gmail.com>
2020-08-17 10:30:40 +0900

    feat(twitter): add followers/list func

--- packages/functions/src/modules/twitter/client.ts
+++ packages/functions/src/modules/twitter/client.ts
@@ -1,6 +1,34 @@
 import * as functions from 'firebase-functions';
 import * as Twitter from 'twitter';
 
+/**
+ * @see https://developer.twitter.com/en/docs/twitter-api/v1/data-dictionary/overview/user-object
+ */
+export type TwitterUser = {
+  id: number;
+  id_str: string;
+  name: string;
+  screen_name: string;
+  location: string;
+  derived: any;
+  url: string;
+  description: string;
+  protected: boolean;
+  verified: boolean;
+  followers_count: number;
+  friends_count: number;
+  listed_count: number;
+  favourites_count: number;
+  statuses_count: number;
+  created_at: string;
+  profile_banner_url: string;
+  profile_image_url_https: string;
+  default_profile: boolean;
+  default_profile_image: boolean;
+  withheld_in_countries: string[];
+  withheld_scope: string;
+};
+
 export type AccessToken = Pick<Twitter.AccessTokenOptions, 'access_token_key' | 'access_token_secret'>;
 
 export type TwitterApiResponseType<T> = Promise<{ success: true; data: T } | { success: false; error: any }>;
--- packages/functions/src/modules/twitter/users.ts
+++ packages/functions/src/modules/twitter/users.ts
@@ -1,4 +1,4 @@
-import { getClient, AccessToken, TwitterApiResponseType } from './client';
+import { getClient, TwitterUser, AccessToken, TwitterApiResponseType } from './client';
 
 export type GetFollowersIdsParams =
   | {
@@ -84,6 +84,102 @@ export const getFollowersIdsLoop = async <T extends number | string = number>(
       data: {
         ...resultData,
         ids,
+      },
+    };
+  }
+
+  return {
+    success: false,
+    error,
+  };
+};
+
+export type GetFollowersListParams =
+  | {
+      user_id: string | number;
+      cursor?: string | number;
+      count?: number;
+      skip_status?: boolean;
+      include_user_entities?: boolean;
+    }
+  | {
+      screen_name: string;
+      cursor?: string | number;
+      count?: number;
+      skip_status?: boolean;
+      include_user_entities?: boolean;
+    };
+
+export type GetFollowersListResponse<T extends number | string = number> = {
+  users: TwitterUser[];
+  next_cursor: number;
+  next_cursor_str: string;
+  previous_cursor: number;
+  previous_cursor_str: string;
+};
+
+/**
+ * フォロワーの一覧を取得
+ *
+ * @see https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/follow-search-get-users/api-reference/get-followers-list
+ */
+export const getFollowersList = async <T extends number | string = number>(
+  params: GetFollowersListParams,
+  accessToken?: AccessToken
+): TwitterApiResponseType<GetFollowersListResponse<T>> => {
+  const client = getClient(accessToken);
+
+  return client
+    .get('followers/list', params)
+    .then((response) => {
+      return {
+        success: true as const,
+        data: response as GetFollowersListResponse<T>,
+      };
+    })
+    .catch((error) => {
+      return {
+        success: false as const,
+        error,
+      };
+    });
+};
+
+export const getFollowersListLoop = async <T extends number | string = number>(
+  params: GetFollowersListParams,
+  accessToken?: AccessToken
+): TwitterApiResponseType<GetFollowersListResponse<T>> => {
+  const users: GetFollowersListResponse<T>['users'] = [];
+  let resultData: GetFollowersListResponse<T> | null = null;
+  let error: any | null = null;
+
+  let cursor: string | number = params.cursor || '-1';
+  let loopCount = 0;
+  const maxLoopCount = Math.min(Math.ceil((params.count || 200) / 200), 15);
+
+  while (loopCount < maxLoopCount) {
+    const result = await getFollowersList<T>({ ...params, cursor, count: 200 }, accessToken);
+    if (!result.success) {
+      error = result.error;
+      break;
+    }
+
+    users.push(...result.data.users);
+    resultData = result.data;
+    cursor = result.data.next_cursor_str;
+    loopCount++;
+
+    if (cursor === '0') {
+      break;
+    }
+  }
+
+  if (resultData) {
+    return {
+      success: true,
+      data: {
+        ...resultData,
+        users,
       },
     };
   }
