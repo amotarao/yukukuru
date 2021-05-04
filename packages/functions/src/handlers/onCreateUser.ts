f6bc87dc62c26121658a7c30ef4dffa814fb7f62
Amon Sawamura <amon.sawamura@gmail.com>
2021-05-04 18:30:26 +0900

    disable initialize to create user

--- packages/functions/src/handlers/onCreateUser.ts
+++ packages/functions/src/handlers/onCreateUser.ts
@@ -1,13 +1,8 @@
-import { initializeUser } from '../utils/firestore/users';
 import { AuthOnCreateHandler } from '../types/functions';
-
-type Data = Parameters<typeof initializeUser>[1];
+import { auth } from '../modules/firebase';
 
 export const onCreateUserHandler: AuthOnCreateHandler = async (user) => {
-  const { photoURL, displayName, uid } = user;
-  const data: Data = {
-    displayName: displayName || '',
-    photoUrl: photoURL || '',
-  };
-  await initializeUser(uid, data);
+  console.log(`⚙️ Initializing user document for [${user.uid}]`);
+  await auth.deleteUser(user.uid);
+  console.error(`❗️[Error]: Failed to initialize user for [${user.uid}]: Stopping to create new user.`);
 };
