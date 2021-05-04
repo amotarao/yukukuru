85e9b6c4825367eb28f1fc30bc500a80b4211c73
Amon Sawamura <amon.sawamura@gmail.com>
2021-05-04 19:27:22 +0900

    separate .prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,18 +10,6 @@ module.exports = {
   ],
   rules: {
     '@typescript-eslint/camelcase': 'warn',
-    'prettier/prettier': [
-      'error',
-      {
-        printWidth: 120,
-        tabWidth: 2,
-        useTabs: false,
-        semi: true,
-        singleQuote: true,
-        trailingComma: 'es5',
-        bracketSpacing: true,
-        arrowParens: 'always',
-      },
-    ],
+    'prettier/prettier': 'error',
   },
 };
