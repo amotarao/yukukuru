86a04cab9a22e3664d953ba2bc8cc25fc224abe6
Amon Sawamura <amon.sawamura@gmail.com>
2021-05-05 21:49:48 +0900

    to ts, remove unused

---
+++ packages/admin-app/src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import type { AppProps } from 'next/app';
+import React from 'react';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
