1a36be73c415ddcae7229ed49ffc81576845baba
Amon Sawamura <amon.sawamura@gmail.com>
2021-05-05 13:28:55 +0900

    remove jsx

--- packages/app/src/components/pages/TopPage/TopPage.tsx
+++ packages/app/src/components/pages/TopPage/TopPage.tsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import { jsx, css } from '@emotion/react';
+import { css } from '@emotion/react';
 import Button, { ButtonProps } from '@material-ui/core/Button';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
