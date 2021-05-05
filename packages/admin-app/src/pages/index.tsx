adf651a174aac41b2cfa75247a2f247670b0aea0
Amon Sawamura <amon.sawamura@gmail.com>
2021-05-05 21:50:46 +0900

    remove style

--- packages/admin-app/src/pages/index.tsx
+++ packages/admin-app/src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import React from 'react';
-import styles from '../styles/Home.module.css';
 
 const Home: React.FC = () => {
   return (
