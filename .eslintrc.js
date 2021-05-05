ee0088a77642b93b130491e081f8012830e0996d
Amon Sawamura <amon.sawamura@gmail.com>
2021-05-05 13:19:20 +0900

    use import

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true,
   },
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'import'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
@@ -13,5 +13,13 @@ module.exports = {
   ],
   rules: {
     'prettier/prettier': 'error',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ]
   },
 };
