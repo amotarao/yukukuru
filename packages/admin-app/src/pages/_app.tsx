32a3ab95b031bb4e48b5ad6acf93308a18e01566
Amon Sawamura <amon.sawamura@gmail.com>
2021-05-05 21:54:43 +0900

    ready material-ui

--- packages/admin-app/src/pages/_app.tsx
+++ packages/admin-app/src/pages/_app.tsx
@@ -1,8 +1,22 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
 import type { AppProps } from 'next/app';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
