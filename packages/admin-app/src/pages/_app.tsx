99663b41ec1623f018d8c23a4554fc8c7f508149
Amon Sawamura <amon.sawamura@gmail.com>
2021-05-05 22:17:16 +0900

    add AppBar

--- packages/admin-app/src/pages/_app.tsx
+++ packages/admin-app/src/pages/_app.tsx
@@ -1,10 +1,16 @@
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import IconButton from '@material-ui/core/IconButton';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
 import type { AppProps } from 'next/app';
 import React, { useEffect } from 'react';
+import styles from '../styles/app.module.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   useEffect(() => {
-    // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
@@ -14,6 +20,17 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={styles.menu_button} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={styles.title}>
+            Yukukuru Admin
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
       <Component {...pageProps} />
     </>
   );
