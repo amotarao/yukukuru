3578cbc19fce9d899104d0a0b3e8fee9aade311f
Amon Sawamura <amon.sawamura@gmail.com>
2021-05-05 10:00:05 +0900

    root

--- packages/functions/.eslintrc.js
+++ packages/functions/.eslintrc.js
@@ -1,17 +1,3 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  rules: {
-    'prettier/prettier': 'error',
-  },
+  extends: ['../../.eslintrc.js'],
 };
