4438bbf251064ce549c63833006a4a2acc18c1c2
Amon Sawamura <amon.sawamura@gmail.com>
2021-05-07 11:01:22 +0900

    change interval

--- packages/functions/src/handlers/handlers/updateTwUsers.ts
+++ packages/functions/src/handlers/handlers/updateTwUsers.ts
@@ -7,7 +7,7 @@ import { log } from '../../utils/log';
 
 export const updateTwUsersHandler: PubSubOnRunHandler = async () => {
   const now = new Date(Math.floor(new Date().getTime() / (60 * 1000)) * 60 * 1000);
-  const group = getGroupFromTime(12, now);
+  const group = getGroupFromTime(1, now);
 
   // 3日前
   const previous = new Date(now.getTime() - 3 * 24 * 60 * 60 * 1000 + 60 * 1000);
