9c19b96eddad25d499fa6f239278e119843c903c
Amon Sawamura <amon.sawamura@gmail.com>
2021-05-08 01:42:25 +0900

    move function

--- packages/functions/src/modules/firestore/users/index.ts
+++ packages/functions/src/modules/firestore/users/index.ts
@@ -1,28 +1,6 @@
-import { FirestoreDateLike, UserData } from '@yukukuru/types';
-import { firestore, admin } from '../../firebase';
-import { getGroupIndex } from '../../group';
+import { firestore } from '../../firebase';
 
 const collection = firestore.collection('users');
-
-/**
- * ユーザーを初期化
- */
-export async function initializeUser(id: string, props: Pick<UserData, 'photoUrl' | 'displayName'>): Promise<void> {
-  const now = admin.firestore.FieldValue.serverTimestamp();
-
-  const data: UserData<FirestoreDateLike> = {
-    active: true,
-    lastUpdated: now,
-    lastUpdatedTwUsers: now,
-    lastUpdatedCheckIntegrity: now,
-    nextCursor: '-1',
-    currentWatchesId: '',
-    pausedGetFollower: false,
-    group: getGroupIndex(id),
-    ...props,
-  };
-  await collection.doc(id).set(data, { merge: true });
-}
 
 /**
  * ユーザーの active を true にする
