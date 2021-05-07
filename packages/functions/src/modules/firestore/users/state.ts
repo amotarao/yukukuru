d816523b18c3dea3abebeabd28ed9c3386ceca43
Amon Sawamura <amon.sawamura@gmail.com>
2021-05-08 01:48:20 +0900

    move func

--- packages/functions/src/modules/firestore/users/state.ts
+++ packages/functions/src/modules/firestore/users/state.ts
@@ -1,6 +1,8 @@
 import { FirestoreDateLike, UserData } from '@yukukuru/types';
 import { firestore } from '../../firebase';
 
+const usersCollection = firestore.collection('users');
+
 export const setUserResult = async (userId: string, watchId: string, nextCursor: string, date: Date): Promise<void> => {
   const collection = firestore.collection('users').doc(userId);
   const ended = nextCursor === '0' || nextCursor === '-1';
@@ -33,4 +35,12 @@ export const updateUserLastUpdatedTwUsers = async (userId: string, date: Date): 
   };
 
   await collection.update(data);
+};
+
+export const updateUserCheckIntegrity = async (uid: string, date: Date): Promise<void> => {
+  const doc = usersCollection.doc(uid);
+  const data: Pick<UserData<FirestoreDateLike>, 'lastUpdatedCheckIntegrity'> = {
+    lastUpdatedCheckIntegrity: date,
+  };
+  await doc.update(data);
 };
