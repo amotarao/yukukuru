ab31dfd010de28c441e1020c17bf10380bb49f14
Amon Sawamura <amon.sawamura@gmail.com>
2021-05-08 10:03:31 +0900

    summarize var

--- packages/functions/src/modules/firestore/tokens/index.ts
+++ packages/functions/src/modules/firestore/tokens/index.ts
@@ -2,19 +2,21 @@ import { TokenData } from '@yukukuru/types';
 import { firestore } from '../../firebase';
 import { setUserToNotActive } from '../users/active';
 
+const collection = firestore.collection('tokens');
+
 export const setTokenInvalid = async (userId: string): Promise<void> => {
   const user = setUserToNotActive(userId);
   const data: Pick<TokenData, 'twitterAccessToken' | 'twitterAccessTokenSecret'> = {
     twitterAccessToken: '',
     twitterAccessTokenSecret: '',
   };
-  const token = firestore.collection('tokens').doc(userId).update(data);
+  const token = collection.doc(userId).update(data);
 
   await Promise.all([user, token]);
 };
 
 export const getToken = async (userId: string): Promise<TokenData | null> => {
-  const tokenRef = firestore.collection('tokens').doc(userId);
+  const tokenRef = collection.doc(userId);
   const tokenDoc = await tokenRef.get();
   if (!tokenDoc.exists) {
     return null;
