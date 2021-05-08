67d579535dc7218f73def2d2f0a7c70042ed7bd7
Amon Sawamura <amon.sawamura@gmail.com>
2021-05-09 03:39:29 +0900

    separate

---
+++ packages/functions/src/modules/twitter/users/lookup.ts
@@ -0,0 +1,69 @@
+import * as _ from 'lodash';
+import * as Twitter from 'twitter';
+import { TwitterClientError, twitterClientErrorHandler } from '../error';
+
+export type TwitterGetUsersLookupParameters = {
+  usersId: string[];
+};
+
+/**
+ * ユーザー情報を取得
+ * 100人まで 取得可能
+ * 15分につき 300回 実行可能
+ */
+const getUsersLookupSingle = (
+  client: Twitter,
+  { usersId }: TwitterGetUsersLookupParameters
+): Promise<{ response: TwitterUserInterface[] } | { errors: TwitterClientError[] }> => {
+  return client
+    .get('users/lookup', {
+      user_id: usersId.join(','),
+    })
+    .then((response) => {
+      return { response: response as TwitterUserInterface[] };
+    })
+    .catch(twitterClientErrorHandler);
+};
+
+/**
+ * ユーザー情報を取得
+ * 15分につき 30,000人まで 取得可能
+ */
+export const getUsersLookup = async (
+  client: Twitter,
+  { usersId }: TwitterGetUsersLookupParameters
+): Promise<{ response: TwitterUserInterface[] } | { errors: TwitterClientError[] }> => {
+  const users: TwitterUserInterface[] = [];
+  const errors: TwitterClientError[] = [];
+
+  const lookup = _.chunk(_.uniq(usersId), 100).map(async (usersId) => {
+    const result = await getUsersLookupSingle(client, { usersId });
+
+    if ('errors' in result) {
+      errors.push(...result.errors);
+      return;
+    }
+
+    result.response.forEach((res) => {
+      const { id_str, screen_name, name, profile_image_url_https, followers_count, verified } = res;
+      const data: TwitterUserInterface = {
+        id_str,
+        screen_name,
+        name,
+        profile_image_url_https,
+        followers_count,
+        verified,
+      };
+      users.push(data);
+    });
+    return;
+  });
+
+  await Promise.all(lookup);
+
+  if (users.length || !errors.length) {
+    return { response: users };
+  }
+
+  return { errors };
+};
