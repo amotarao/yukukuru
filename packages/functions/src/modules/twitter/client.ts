bf80fa021542723c9ac2ab3c1cbf30b606cce9d4
Amon Sawamura <amon.sawamura@gmail.com>
2021-05-08 10:17:13 +0900

    add ?

--- packages/functions/src/modules/twitter/client.ts
+++ packages/functions/src/modules/twitter/client.ts
@@ -1,7 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as Twitter from 'twitter';
 
-export const getClient = (options: Partial<Twitter.AccessTokenOptions>): Twitter => {
+export const getClient = (options?: Partial<Twitter.AccessTokenOptions>): Twitter => {
   const defaultOptions = functions.config().twitter as Twitter.AccessTokenOptions;
   return new Twitter({
     ...defaultOptions,
