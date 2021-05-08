149b9f70bcf1d6d3466d5c661cfb46a82f3ecbdf
Amon Sawamura <amon.sawamura@gmail.com>
2021-05-08 10:31:58 +0900

    add setUserToActive

--- packages/functions/src/handlers/handlers/onUpdateToken.ts
+++ packages/functions/src/handlers/handlers/onUpdateToken.ts
@@ -1,13 +1,21 @@
 import { TokenData } from '@yukukuru/types';
+import { setUserToActive } from '../../modules/firestore/users/active';
 import { updateUserInvalidToken, updateUserValidToken } from '../../modules/firestore/users/validToken';
 import { FirestoreOnUpdateHandler } from '../../types/functions';
 
 export const onUpdateTokenHandler: FirestoreOnUpdateHandler = async ({ after }) => {
+  const uid = after.id;
+
   const { twitterAccessToken, twitterAccessTokenSecret, twitterId } = after.data() as TokenData;
   const invalid = !twitterAccessToken || !twitterAccessTokenSecret || !twitterId;
+
   if (invalid) {
-    await updateUserInvalidToken(after.id);
+    await updateUserInvalidToken(uid);
   } else {
-    await updateUserValidToken(after.id);
+    await updateUserValidToken(uid);
+
+    // レガシー対応
+    // validToken を active で管理していた時期の名残
+    await setUserToActive(uid);
   }
 };
