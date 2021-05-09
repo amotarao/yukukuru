3ec330e1f2743af98326e04e73d03c890a2bd90b
Amon Sawamura <amon.sawamura@gmail.com>
2021-05-09 21:47:59 +0900

    ProfileImage: watch change src

--- packages/app/src/components/atoms/ProfileImage/ProfileImage.tsx
+++ packages/app/src/components/atoms/ProfileImage/ProfileImage.tsx
@@ -1,16 +1,23 @@
-/** @jsxImportSource @emotion/react */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const fallbackImage = 'https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png';
 
 export type ProfileImageProps = React.HTMLProps<HTMLImageElement>;
 
-export const ProfileImage: React.FC<ProfileImageProps> = (props) => {
-  const [src, setSrc] = useState<ProfileImageProps['src']>(props.src);
+export const ProfileImage: React.FC<ProfileImageProps> = ({ src, ...props }) => {
+  const [stateSrc, setStateSrc] = useState<ProfileImageProps['src']>(src);
+  const [error, setError] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (stateSrc !== src) {
+      setStateSrc(src);
+      setError(false);
+    }
+  }, [stateSrc, src]);
 
   const onError = () => {
-    setSrc(fallbackImage);
+    setError(true);
   };
 
-  return <img {...props} src={src} alt={props.alt} crossOrigin="anonymous" onError={onError} />;
+  return <img {...props} src={!error ? stateSrc : fallbackImage} crossOrigin="anonymous" onError={onError} />;
 };
