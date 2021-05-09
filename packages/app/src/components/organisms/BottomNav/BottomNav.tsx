acb1423103cf7e4830824cede0af28a9035aaba0
Amon Sawamura <amon.sawamura@gmail.com>
2021-05-09 20:55:17 +0900

    to css modules

--- packages/app/src/components/organisms/BottomNav/BottomNav.tsx
+++ packages/app/src/components/organisms/BottomNav/BottomNav.tsx
@@ -1,9 +1,8 @@
-/** @jsxImportSource @emotion/react */
 import HomeIcon from '@material-ui/icons/Home';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import SettingsIcon from '@material-ui/icons/Settings';
 import React from 'react';
-import { style } from './style';
+import styles from './styles.module.scss';
 
 export type NavType = 'home' | 'notification' | 'setting';
 
@@ -24,22 +23,27 @@ export const BottomNav: React.FC<BottomNavProps> = ({ active, onChange }) => {
   };
 
   return (
-    <nav css={style.nav}>
-      <ul css={style.list}>
-        <li css={style.item}>
-          <button css={style.button} aria-selected={active === 'home'} value="home" onClick={onClick}>
+    <nav className={styles.nav}>
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <button className={styles.button} aria-selected={active === 'home'} value="home" onClick={onClick}>
             <HomeIcon />
             ホーム
           </button>
         </li>
-        <li css={style.item}>
-          <button css={style.button} aria-selected={active === 'notification'} value="notification" onClick={onClick}>
+        <li className={styles.item}>
+          <button
+            className={styles.button}
+            aria-selected={active === 'notification'}
+            value="notification"
+            onClick={onClick}
+          >
             <NotificationsIcon />
             お知らせ
           </button>
         </li>
-        <li css={style.item}>
-          <button css={style.button} aria-selected={active === 'setting'} value="setting" onClick={onClick}>
+        <li className={styles.item}>
+          <button className={styles.button} aria-selected={active === 'setting'} value="setting" onClick={onClick}>
             <SettingsIcon />
             設定
           </button>
