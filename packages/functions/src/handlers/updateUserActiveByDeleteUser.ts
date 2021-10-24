0d84f87b98776295c00051e4826536e59a548593
Amon Sawamura <amon.sawamura@gmail.com>
2021-10-24 11:38:47 +0900

    setUserDeletedAuth

--- packages/functions/src/handlers/updateUserActiveByDeleteUser.ts
+++ packages/functions/src/handlers/updateUserActiveByDeleteUser.ts
@@ -1,10 +1,10 @@
-import { setUserToNotActive } from '../modules/firestore/users/active';
+import { setUserDeletedAuth } from '../modules/firestore/users/deletedAuth';
 import { existsUserDocument } from '../modules/firestore/users/exists';
 import { AuthOnDeleteHandler } from '../types/functions';
 
 export const updateUserActiveByDeleteUserHandler: AuthOnDeleteHandler = async ({ uid }) => {
   const exists = await existsUserDocument(uid);
   if (exists) {
-    await setUserToNotActive(uid);
+    await setUserDeletedAuth(uid);
   }
 };
