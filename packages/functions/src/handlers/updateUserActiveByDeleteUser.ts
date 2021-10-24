d3a4b35061636022db847099a24e4c4d3e70dc07
Amon Sawamura <amon.sawamura@gmail.com>
2021-10-24 11:38:42 +0900

    rename

--- packages/functions/src/handlers/updateUserActiveByDeleteUser.ts
+++ packages/functions/src/handlers/updateUserActiveByDeleteUser.ts
@@ -1,9 +1,9 @@
 import { setUserToNotActive } from '../modules/firestore/users/active';
-import { existsUserDoc } from '../modules/firestore/users/exists';
+import { existsUserDocument } from '../modules/firestore/users/exists';
 import { AuthOnDeleteHandler } from '../types/functions';
 
 export const updateUserActiveByDeleteUserHandler: AuthOnDeleteHandler = async ({ uid }) => {
-  const exists = await existsUserDoc(uid);
+  const exists = await existsUserDocument(uid);
   if (exists) {
     await setUserToNotActive(uid);
   }
