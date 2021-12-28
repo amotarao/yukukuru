039d9a42fee684e89b60637df126e3b86a98aabb
Amon Sawamura <amon.sawamura@gmail.com>
2021-12-28 10:11:59 +0900

    type

--- packages/admin-app/src/pages/_app.tsx
+++ packages/admin-app/src/pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css';
+import React from 'react';
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
-}
+};
 
 export default MyApp;
