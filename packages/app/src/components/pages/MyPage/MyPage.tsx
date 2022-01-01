4e3ed21b78de731b6d70d92a4da7312ab1389100
amon <amon.sawamura@gmail.com>
2022-01-01 21:49:12 +0900

    fix(app): 色指定修正 (#240)

--- packages/app/src/components/pages/MyPage/MyPage.tsx
+++ packages/app/src/components/pages/MyPage/MyPage.tsx
@@ -76,13 +76,13 @@ const Home: React.FC<Pick<MyPageProps, 'items' | 'lastRunnedGetFollowers'>> = ({
       <nav className={styles.labelNav}>
         <ul className="flex justify-between sm:justify-around">
           <li
-            className="inline-block px-3 py-1 rounded sm:rounded-full border-l-4 border-l-yuku sm:border-l-0 bg-white sm:bg-yuku text-xs"
+            className="inline-block px-3 py-1 rounded sm:rounded-full border-l-4 border-l-yuku sm:border-l-0 bg-back sm:bg-yuku text-xs"
             data-type="yuku"
           >
             ゆくひと
           </li>
           <li
-            className="inline-block px-3 py-1 rounded sm:rounded-full border-r-4 border-r-kuru sm:border-r-0 bg-white sm:bg-kuru text-xs"
+            className="inline-block px-3 py-1 rounded sm:rounded-full border-r-4 border-r-kuru sm:border-r-0 bg-back sm:bg-kuru text-xs"
             data-type="kuru"
           >
             くるひと
