ceb06b336bce16a84266e559232e916ad632aa8d
Amon Sawamura <amon.sawamura@gmail.com>
2022-01-01 14:31:06 +0900

    add color theme

--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
   plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        main: 'var(--main)',
+        sub: 'var(--sub)',
+        back: 'var(--back)',
+        back2: 'var(--back-2)',
+        shadow: 'var(--shadow)',
+        backShadow: 'var(--back-shadow)',
+        primary: 'var(--primary)',
+        primaryBg: 'var(--primary-bg)',
+        primaryGradient: 'var(--primary-gradient)',
+        yuku: 'var(--yuku)',
+        kuru: 'var(--kuru)',
+        danger: 'var(--danger)',
+      },
+    },
+  },
 };
