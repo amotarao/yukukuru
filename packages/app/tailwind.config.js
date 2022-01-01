7f65365675429a1e80fba6c9fa5bd737bcc18d13
Amon Sawamura <amon.sawamura@gmail.com>
2022-01-01 19:31:04 +0900

    add bgimage

--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -2,24 +2,27 @@ module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   plugins: [require('@tailwindcss/line-clamp')],
   theme: {
+    screens: {
+      sm: '640px',
+    },
     extend: {
       colors: {
         main: 'var(--main)',
         sub: 'var(--sub)',
         back: 'var(--back)',
-        back2: 'var(--back-2)',
+        'back-2': 'var(--back-2)',
         shadow: 'var(--shadow)',
-        backShadow: 'var(--back-shadow)',
+        'back-shadow': 'var(--back-shadow)',
         primary: 'var(--primary)',
-        primaryBg: 'var(--primary-bg)',
-        primaryGradient: 'var(--primary-gradient)',
+        'primary-bg': 'var(--primary-bg)',
+        'primary-gradient': 'var(--primary-gradient)',
         yuku: 'var(--yuku)',
         kuru: 'var(--kuru)',
         danger: 'var(--danger)',
       },
-    },
-    screens: {
-      sm: '640px',
+      backgroundImage: {
+        'top-bg': 'var(--primary-gradient)',
+      },
     },
   },
 };
