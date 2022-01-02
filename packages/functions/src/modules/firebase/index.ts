fc3dfd80304eec7bb0999d16b2ec01ab80d5df8a
Amon Sawamura <amon.sawamura@gmail.com>
2022-01-03 00:32:44 +0900

    add storage settings

--- packages/functions/src/modules/firebase/index.ts
+++ packages/functions/src/modules/firebase/index.ts
@@ -2,9 +2,10 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
+  storageBucket: functions.config().storage_bucket.default_bucket as string,
 });
 
-const auth = admin.auth();
-const firestore = admin.firestore();
-
-export { auth, firestore };
+export const auth = admin.auth();
+export const firestore = admin.firestore();
+export const storage = admin.storage();
+export const bucket = storage.bucket();
