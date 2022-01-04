251495e17df30e1c516a6ffacb1d0372ef0a322b
Amon Sawamura <amon.sawamura@gmail.com>
2022-01-05 07:34:49 +0900

    isSupported

--- packages/app/src/modules/firebase.ts
+++ packages/app/src/modules/firebase.ts
@@ -1,4 +1,4 @@
-import { getAnalytics } from 'firebase/analytics';
+import { getAnalytics, isSupported } from 'firebase/analytics';
 import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
@@ -17,4 +17,4 @@ const app = getApps().length
 
 export const auth = getAuth(app);
 export const firestore = getFirestore(app);
-export const analytics = getAnalytics(app);
+export const analytics = isSupported() ? getAnalytics(app) : null;
