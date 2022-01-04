ceec6d987ea84362f7d844608f2d69fda16e399e
Amon Sawamura <amon.sawamura@gmail.com>
2022-01-05 03:25:55 +0900

    NEXT_PUBLIC_

--- packages/app/src/modules/firebase.ts
+++ packages/app/src/modules/firebase.ts
@@ -6,13 +6,13 @@ import { getFirestore } from 'firebase/firestore';
 const app = getApps().length
   ? getApp()
   : initializeApp({
-      apiKey: process.env.NEXT_APP_FIREBASE_API_KEY,
-      authDomain: process.env.NEXT_APP_FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.NEXT_APP_FIREBASE_DATABASE_URL,
-      projectId: process.env.NEXT_APP_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.NEXT_APP_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.NEXT_APP_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.NEXT_APP_FIREBASE_APP_ID,
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
     });
 
 export const auth = getAuth(app);
