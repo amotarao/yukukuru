279b1ed7bb19f9a2a9b773cce7a70e9fa9e6cd69
Amon Sawamura <amotarao@gmail.com>
2022-10-27 16:09:43 +0900

    add returnUrl

--- packages/app/src/modules/functions/stripe.ts
+++ packages/app/src/modules/functions/stripe.ts
@@ -3,6 +3,8 @@ import { functions } from '../firebase';
 
 export const getPortalLink = async (): Promise<string> => {
   const functionRef = httpsCallable(functions, 'ext-firestore-stripe-subscriptions-createPortalLink');
-  const { data } = (await functionRef({ returnUrl: window.location.origin })) as { data: { url: string } };
+  const { data } = (await functionRef({ returnUrl: window.location.origin + '/supporter' })) as {
+    data: { url: string };
+  };
   return data.url;
 };
