a754c10fad4205d5fb8fbaef06a4749f18c7c7a1
Amon Sawamura <amotarao@gmail.com>
2022-10-31 23:37:38 +0900

    add cleanUsers handler

---
+++ packages/functions/src/handlers/cleanUsers/_pubsub.ts
@@ -0,0 +1,18 @@
+export const topicName = 'cleanUsers';
+
+export type Message = {
+  /** Firebase UID */
+  uid: string;
+
+  /** 有効かどうか */
+  active: boolean;
+
+  /** auth が削除されているかどうか */
+  deletedAuth: boolean;
+
+  /** フォロワー一覧取得 最終実行日時 */
+  lastUpdated: Date | string;
+
+  /** 送信日時 */
+  publishedAt: Date | string;
+};
---
+++ packages/functions/src/handlers/cleanUsers/publish.ts
@@ -0,0 +1,50 @@
+import { UserData } from '@yukukuru/types';
+import dayjs = require('dayjs');
+import * as functions from 'firebase-functions';
+import { firestore } from '../../modules/firebase';
+import { getGroupFromTime } from '../../modules/group';
+import { publishMessages } from '../../modules/pubsub/publish';
+import { Message, topicName } from './_pubsub';
+
+/**
+ * ユーザーデータ削除 定期実行
+ *
+ * 処理を実行するかどうかは run でチェック
+ *
+ * 12分おきに実行
+ * グループ毎に 3時間おきに実行
+ */
+export const publish = functions
+  .region('asia-northeast1')
+  .runWith({
+    timeoutSeconds: 10,
+    memory: '256MB',
+  })
+  .pubsub.schedule('*/12 * * * *')
+  .timeZone('Asia/Tokyo')
+  .onRun(async (context) => {
+    const now = new Date(context.timestamp);
+
+    // 対象ユーザーの取得
+    // 実行するかどうかは run で確認
+    const group = getGroupFromTime(12, now);
+
+    const snapshot = await firestore
+      .collection('users')
+      .where('lastUpdated', '<=', dayjs(group).subtract(30, 'day').toDate())
+      .where('group', '==', group)
+      .select('active', 'deletedAuth', 'lastUpdated')
+      .get();
+
+    // publish データ作成・送信
+    const messages: Message[] = snapshot.docs.map((doc) => ({
+      uid: doc.id,
+      active: doc.get('active') as UserData['active'],
+      deletedAuth: doc.get('deletedAuth') as UserData['deletedAuth'],
+      lastUpdated: (doc.get('lastUpdated') as UserData['lastUpdated']).toDate(),
+      publishedAt: now,
+    }));
+    await publishMessages(topicName, messages);
+
+    console.log(`✔️ Completed publish ${messages.length} message.`);
+  });
