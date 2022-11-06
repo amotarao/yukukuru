224e8eada1529da132ab531e505422822561c79c
amon <amotarao@gmail.com>
2022-11-06 10:38:53 +0900

    feat(functions): Twitter Client リプレイス (#326)
    * install twitter-api-v2
    
    * uninstall twitter
    
    * use twitter-api-v2
    
    * uninstall @types/twitter
    
    * use hasErrorCode
    
    * remove document

--- packages/functions/src/modules/twitter/account/verifyCredentials.ts
+++ packages/functions/src/modules/twitter/account/verifyCredentials.ts
@@ -1,13 +1,13 @@
-import * as Twitter from 'twitter';
-import { TwitterUserObject } from '..';
-import { TwitterClientError, twitterClientErrorHandler } from '../error';
+import { ApiResponseError, TwitterApiReadOnly } from 'twitter-api-v2';
+import { TwitterUser } from '..';
+import { twitterClientErrorHandler } from '../error';
 
 /**
  * @see https://developer.twitter.com/en/docs/twitter-api/v1/accounts-and-users/manage-account-settings/api-reference/get-account-verify_credentials
  */
 export const getAccountVerifyCredentials = (
-  client: Twitter
-): Promise<{ response: TwitterUserObject } | { errors: TwitterClientError[] }> => {
+  client: TwitterApiReadOnly
+): Promise<{ response: TwitterUser } | { error: ApiResponseError }> => {
   return client
     .get('account/verify_credentials', {
       include_entities: true,
@@ -16,7 +16,7 @@ export const getAccountVerifyCredentials = (
     })
     .then((res) => {
       const { id_str, screen_name, name, profile_image_url_https, followers_count, verified } = res;
-      const response: TwitterUserObject = {
+      const response: TwitterUser = {
         id_str,
         screen_name,
         name,
