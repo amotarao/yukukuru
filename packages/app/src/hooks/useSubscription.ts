19d22fc20f5ce5b9ada037d3e9b07c2fdd44b3e1
amon <amotarao@gmail.com>
2022-11-13 23:35:11 +0900

    fix(app): supporter: おすすめテキスト変更 (#354)
    * fix(app): supporter: おすすめテキスト変更
    
    * fix text

--- packages/app/src/hooks/useSubscription.ts
+++ packages/app/src/hooks/useSubscription.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getOwnActiveSubscriptions } from '../modules/firestore/stripe';
 import { useAuth } from './auth';
 
@@ -10,7 +10,7 @@ type Subscription = {
 export const useSubscription = () => {
   const [{ uid }] = useAuth();
   const [isLoading, setIsLoading] = useState(true);
-  const [subscriptions, setSubscriptions] = useState<Subscription[]>([]);
+  const [isSupporter, setIsSupporter] = useState<boolean | null>(null);
 
   useEffect(() => {
     if (!uid) {
@@ -19,19 +19,15 @@ export const useSubscription = () => {
     }
     setIsLoading(true);
     getOwnActiveSubscriptions(uid).then((querySnapshot) => {
-      setSubscriptions(querySnapshot.docs.map((doc) => doc.data() as Subscription));
+      const subscriptions = querySnapshot.docs.map((doc) => doc.data() as Subscription);
+      const isSupporter = subscriptions.find((subscription) => subscription.role === 'supporter')?.status === 'active';
+      setIsSupporter(isSupporter);
       setIsLoading(false);
     });
   }, [uid]);
 
-  const isSupporter = useMemo(
-    () => subscriptions.find((subscription) => subscription.role === 'supporter')?.status === 'active',
-    [subscriptions]
-  );
-
   return {
     isLoading,
-    subscriptions,
     isSupporter,
   };
 };
