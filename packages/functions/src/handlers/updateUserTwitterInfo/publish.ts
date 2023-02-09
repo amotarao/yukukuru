9916720d1c2bf07eff03edf2f0ba6a5e7ddaed6c
Amon Sawamura <amotarao@gmail.com>
2023-02-09 06:32:20 +0100

    function: limit を外す

--- packages/functions/src/handlers/updateUserTwitterInfo/publish.ts
+++ packages/functions/src/handlers/updateUserTwitterInfo/publish.ts
@@ -1,4 +1,5 @@
 import { UserData } from '@yukukuru/types';
+import * as dayjs from 'dayjs';
 import * as functions from 'firebase-functions';
 import { firestore } from '../../modules/firebase';
 import { getGroupFromTime } from '../../modules/group';
@@ -18,8 +19,8 @@ export const publish = functions
     const now = new Date(context.timestamp);
     const group = getGroupFromTime(1, now);
 
-    // 1日前 (-1m)
-    const previous = new Date(now.getTime() - 1 * 24 * 60 * 60 * 1000 + 60 * 1000);
+    // 3日前
+    const previous = dayjs(now).subtract(3, 'days').subtract(1, 'minutes').toDate();
 
     const usersSnap = await firestore
       .collection('users')
@@ -27,13 +28,13 @@ export const publish = functions
       .where('lastUpdatedUserTwitterInfo', '<', previous)
       .where('group', '==', group)
       .orderBy('lastUpdatedUserTwitterInfo', 'asc')
-      .limit(10)
       .get();
 
     const items: Message[] = usersSnap.docs
       .filter((doc) => !(doc.get('deletedAuth') as UserData['deletedAuth']))
       .map((doc) => doc.id)
-      .map((id) => ({ uid: id, publishedAt: now }));
+      .map((id) => ({ uid: id, publishedAt: now }))
+      .slice(0, 10);
     await publishMessages(topicName, items);
 
     console.log(`✔️ Completed publish ${items.length} message.`);
