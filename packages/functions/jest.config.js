ba7188b9e621aa29feae3634c6ba95e8fee69974
Amon Sawamura <amotarao@gmail.com>
2023-03-30 11:02:47 +0900

    functions: add jest files

---
+++ packages/functions/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  setupFiles: ['<rootDir>/dotenv-config.js'],
+};
