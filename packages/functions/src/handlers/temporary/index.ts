b666fbe709394c963c2a542d466e2f10cdc4c194
Amon Sawamura <amotarao@gmail.com>
2023-03-31 22:08:34 +0900

    functions: add addGetFollowersIdsField temporary function

--- packages/functions/src/handlers/temporary/index.ts
+++ packages/functions/src/handlers/temporary/index.ts
@@ -1,6 +1,7 @@
 import { FieldValue } from 'firebase-admin/firestore';
 import * as functions from 'firebase-functions';
 import { firestore } from '../../modules/firebase';
+import { getGroupFromTime } from '../../modules/group';
 
 export const deleteTwitterIdFieldFromToken = functions
   .region('asia-northeast1')
@@ -20,5 +21,40 @@ export const deleteTwitterIdFieldFromToken = functions
         });
       })
     );
+    console.log(`✔️ Completed ${snapshot.size} document(s).`);
+  });
+
+export const addGetFollowersIdsField = functions
+  .region('asia-northeast1')
+  .runWith({
+    timeoutSeconds: 30,
+    memory: '256MB',
+  })
+  .pubsub.schedule('* * * * *')
+  .timeZone('Asia/Tokyo')
+  .onRun(async (context) => {
+    const now = new Date(context.timestamp);
+    const group = getGroupFromTime(1, now);
+
+    const usersCollection = firestore.collection('users');
+    const snapshot = await usersCollection.where('group', '==', group).get();
+
+    const bulkWriter = firestore.bulkWriter();
+    await Promise.all(
+      snapshot.docs.map(async (user) => {
+        bulkWriter.set(
+          firestore.collection('tokens').doc(user.id),
+          {
+            _lastUsed: {
+              v1_getFollowersIds: new Date(2000, 0, 1),
+              v2_getUserFollowers: new Date(2000, 0, 1),
+              v2_getUsers: new Date(2000, 0, 1),
+            },
+          },
+          { merge: true }
+        );
+      })
+    );
+    await bulkWriter.close();
     console.log(`✔️ Completed ${snapshot.size} document(s).`);
   });
