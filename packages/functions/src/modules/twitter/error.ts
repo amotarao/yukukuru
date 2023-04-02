916d89081994bbd123577c5ea601ae8b5901b226
Amon Sawamura <amotarao@gmail.com>
2023-04-02 21:50:26 +0900

    functions: v1.1 GET /followers/ids で 131 エラーが出ることが多いため対応

--- packages/functions/src/modules/twitter/error.ts
+++ packages/functions/src/modules/twitter/error.ts
@@ -1,4 +1,4 @@
-import { ApiResponseError, EApiV1ErrorCode } from 'twitter-api-v2';
+import { ApiResponseError } from 'twitter-api-v2';
 
 const logError = (error: unknown): void => {
   if (error && error instanceof ApiResponseError) {
@@ -22,32 +22,4 @@ export const twitterClientErrorHandler = (error: unknown): { error: ApiResponseE
   }
 
   throw error;
-};
-
-/**
- * エラーコードを確認
- *
- * @param error エラー
- * @param code エラーコード
- *
- * @see https://developer.twitter.com/ja/docs/basics/response-codes
- */
-const checkError = (error: ApiResponseError, code: number): boolean => {
-  return error.hasErrorCode(code);
-};
-
-export const checkNoUserMatches = (error: ApiResponseError): boolean => {
-  return checkError(error, EApiV1ErrorCode.NoUserMatch);
-};
-
-export const checkRateLimitExceeded = (error: ApiResponseError): boolean => {
-  return checkError(error, EApiV1ErrorCode.RateLimitExceeded);
-};
-
-export const checkInvalidOrExpiredToken = (error: ApiResponseError): boolean => {
-  return checkError(error, EApiV1ErrorCode.InvalidOrExpiredToken);
-};
-
-export const checkTemporarilyLocked = (error: ApiResponseError): boolean => {
-  return checkError(error, EApiV1ErrorCode.AccountLocked);
 };
