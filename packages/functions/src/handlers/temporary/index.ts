da57aa67244c407fd971d608dddf24381992c6bf
Amon Sawamura <amotarao@gmail.com>
2023-04-05 21:49:15 +0900

    functions: watches 削除 temporary 追加

--- packages/functions/src/handlers/temporary/index.ts
+++ packages/functions/src/handlers/temporary/index.ts
@@ -1,5 +1,6 @@
 import { FieldValue } from 'firebase-admin/firestore';
 import * as functions from 'firebase-functions';
+import { firestore } from '../../modules/firebase';
 import { usersCollection } from '../../modules/firestore/users';
 
 export const deleteGetFollowersV1Field = functions
@@ -19,5 +20,24 @@ export const deleteGetFollowersV1Field = functions
         } as any);
       })
     );
+    console.log(`deleted ${snapshot.docs.length} items.`);
+  });
+
+/** watches コレクション 削除 */
+export const deleteWatches = functions
+  .region('asia-northeast1')
+  .runWith({
+    timeoutSeconds: 10,
+    memory: '256MB',
+  })
+  .pubsub.schedule('* * * * *')
+  .timeZone('Asia/Tokyo')
+  .onRun(async () => {
+    const snapshot = await firestore.collectionGroup('watches').limit(300).get();
+    const bulkWriter = firestore.bulkWriter();
+    snapshot.docs.forEach((doc) => {
+      bulkWriter.delete(doc.ref);
+    });
+    await bulkWriter.close();
     console.log(`deleted ${snapshot.docs.length} items.`);
   });
