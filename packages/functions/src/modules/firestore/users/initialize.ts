14d914d3a29d7b920ad5519123d87d345c504848
Amon Sawamura <amotarao@gmail.com>
2023-04-05 09:27:03 +0900

    functions: getFollowersV1 削除

--- packages/functions/src/modules/firestore/users/initialize.ts
+++ packages/functions/src/modules/firestore/users/initialize.ts
@@ -20,12 +20,6 @@ export const initializeUser = async (id: string, twitter: UserTwitter): Promise<
     _checkIntegrityV2Status: {
       lastRun: new Date(0),
     },
-    _getFollowersV1Status: {
-      lastUpdated: new Date(0),
-      nextCursor: '-1',
-      currentWatchesId: '',
-      pausedGetFollower: false,
-    },
   };
   await usersCollection.doc(id).set(data, { merge: true });
 };
