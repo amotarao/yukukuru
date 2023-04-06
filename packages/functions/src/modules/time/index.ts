cb08fc850eade1597592750a21b6f38c33d6097d
Amon Sawamura <amotarao@gmail.com>
2023-04-06 10:32:10 +0900

    functions: checkIntegrityV2 の間隔を調整 role も確認

--- packages/functions/src/modules/time/index.ts
+++ packages/functions/src/modules/time/index.ts
@@ -1,11 +1,21 @@
 import * as dayjs from 'dayjs';
 
-const truncateDate = (date: Date): Date => {
+const truncateDateByMinutes = (date: Date): Date => {
   return new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes());
 };
 
 export const getDiffMinutes = (to: Date, from: Date): number => {
-  const truncatedTo = truncateDate(to);
-  const truncatedFrom = truncateDate(from);
+  const truncatedTo = truncateDateByMinutes(to);
+  const truncatedFrom = truncateDateByMinutes(from);
   return dayjs(truncatedTo).diff(dayjs(truncatedFrom), 'minutes');
+};
+
+const truncateDateByDays = (date: Date): Date => {
+  return new Date(date.getFullYear(), date.getMonth(), date.getDate());
+};
+
+export const getDiffDays = (to: Date, from: Date): number => {
+  const truncatedTo = truncateDateByDays(to);
+  const truncatedFrom = truncateDateByDays(from);
+  return dayjs(truncatedTo).diff(dayjs(truncatedFrom), 'days');
 };
