9c3fe3fbdc7c6b4876e5e13aa84a2f6cd86e6000
Amon Sawamura <amotarao@gmail.com>
2023-04-10 22:29:48 +0900

    app: 設定で setLastViewing 削除

--- packages/app/src/pages/my/settings/index.tsx
+++ packages/app/src/pages/my/settings/index.tsx
@@ -1,38 +1,28 @@
 import Head from 'next/head';
-import { useEffect } from 'react';
 import { LoadingCircle } from '../../../components/atoms/LoadingCircle';
 import { LoginPage } from '../../../components/pages/LoginPage';
 import { SettingsPage } from '../../../components/pages/SettingsPage';
 import { useAuth } from '../../../hooks/auth';
-import { setLastViewing } from '../../../modules/firestore/userStatuses';
 
 const Page: React.FC = () => {
-  const [{ isLoading: authIsLoading, signedIn, signingIn, uid }, { signIn, signOut }] = useAuth();
-
-  // lastViewing 送信
-  useEffect(() => {
-    if (!uid) {
-      return;
-    }
-    setLastViewing(uid);
-  }, [uid]);
+  const [{ isLoading, signedIn, signingIn }, { signIn, signOut }] = useAuth();
 
   return (
     <>
       <Head>
         <title>設定 - ゆくくる beta</title>
       </Head>
-      {authIsLoading || signingIn ? (
+      {isLoading || signingIn ? (
         <LoadingCircle />
-      ) : signedIn ? (
+      ) : !signedIn ? (
+        <LoginPage signIn={signIn} />
+      ) : (
         <SettingsPage
           {...{
             signIn,
             signOut,
           }}
         />
-      ) : (
-        <LoginPage signIn={signIn} />
       )}
     </>
   );
