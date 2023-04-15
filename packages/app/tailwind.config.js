bcb26ec0fdd99fb01fd5eacd567e168cb1195a57
Amon Sawamura <amotarao@gmail.com>
2023-04-16 01:54:31 +0900

    app: setup app dir

--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -1,7 +1,7 @@
 const plugin = require('tailwindcss/plugin');
 
 module.exports = {
-  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: {
       sm: '640px',
